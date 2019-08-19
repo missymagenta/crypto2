1a17ec60e92675150124b9df0769b6a911f50218
missymagenta <alina.soare37@gmail.com>
2019-08-19 20:41:21 +0200

    second commit crypto2

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'firstapp/home.html')
+    import requests
+    import json
+    api_request = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
+    api = json.loads(api_request.content)
+    return render(request, 'firstapp/home.html', {'api': api})
