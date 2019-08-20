638a7f82476181e4f3fb5dc5c07b9a395c012289
missymagenta <alina.soare37@gmail.com>
2019-08-20 17:37:09 +0200

    last commit

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,6 +3,30 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
+
+    # grab prices
+    price_request = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,LTC,XLM,EOS&tsyms=EUR')
+    price = json.loads(price_request.content)
+
+    # grab news
     api_request = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
     api = json.loads(api_request.content)
-    return render(request, 'firstapp/home.html', {'api': api})
+    return render(request, 'firstapp/home.html', {'api': api, 'price':price})
+
+
+def prices(request):
+    import requests
+    import json
+
+    if request.method == 'POST':
+        quote = request.POST['quote'].upper()
+        crypto_request = requests.get(
+            'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + str(quote) + 'S&tsyms=EUR')
+        crypto = json.loads(crypto_request.content)
+
+        return render(request, 'firstapp/prices.html', {'quote': quote, 'crypto': crypto})
+
+
+    else:
+        notfound = True
+        return render(request, 'firstapp/prices.html', {'notfound': notfound})
